9f5ee2d44658b4b1673f1c9ba2380bc2fcad9bff
Craig Huff <craig.huff@tennis-warehouse.com>
2022-01-14 16:06:10 -0800

    Menu Bar App on MacOS

--- Shared/TempusApp.swift
+++ Shared/TempusApp.swift
@@ -10,9 +10,47 @@ import SwiftUI
 @main
 struct TempusApp: App {
     static let shift = Shift()
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    #endif
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(TempusApp.shift)
         }
     }
+
 }
+#if os(macOS)
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var statusItem: NSStatusItem?
+    var popOver = NSPopover()
+    var shift = Shift()
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        if let window = NSApplication.shared.windows.first {
+                    window.close()
+                }
+        
+        let contentView = ContentView().environmentObject(shift)
+        
+        popOver.behavior = .transient
+        popOver.animates = true
+        popOver.contentViewController = NSViewController()
+        popOver.contentViewController?.view = NSHostingView(rootView: contentView)
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let MenuButton = statusItem?.button{
+            MenuButton.image =  NSImage(systemSymbolName: "clock.badge.checkmark", accessibilityDescription: nil)
+            MenuButton.action = #selector(MenuButtonToggle)
+        }
+    }
+    @objc func MenuButtonToggle(){
+        if let menuButton = statusItem?.button{
+            
+            self.popOver.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
+        }
+    }
+}
+#endif
