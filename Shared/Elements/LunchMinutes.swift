683e6c06460c529520e31bcc7f9471b4ef7145b8
Craig Huff <craig.huff@tennis-warehouse.com>
2022-01-17 11:10:29 -0800

    Didn't like the "hack-y" way that it worked, and the look was a little off. But I'll keep it in here in case I want to come back to it

--- Shared/Elements/LunchMinutes.swift
+++ Shared/Elements/LunchMinutes.swift
@@ -12,16 +12,16 @@ struct LunchMinutes: View {
     var body: some View {
         HStack{
             Text("How long was lunch?")
-            TestTextfield(text: $shift.lunchLength, keyType: UIKeyboardType.phonePad)
-                        .frame(maxWidth: 40, minHeight: 0, maxHeight: 50)
-                        .font(.title3)
-//            TextField("30", text: $shift.lunchLength)
-//                .frame(width: 40)
-//                .padding(.leading)
-//                #if os(iOS)
-//                .keyboardType(.numbersAndPunctuation)
-//                .submitLabel(.done)
-//                #endif
+//            TestTextfield(text: $shift.lunchLength, keyType: UIKeyboardType.decimalPad)
+//                        .frame(maxWidth: 40, minHeight: 0, maxHeight: 30)
+//                        .font(.headline)
+            TextField("30", text: $shift.lunchLength)
+                .frame(width: 40)
+                .padding(.leading)
+                #if os(iOS)
+                .keyboardType(.numbersAndPunctuation)
+                .submitLabel(.done)
+                #endif
                 
             Text("minutes")
         }
@@ -36,31 +36,34 @@ struct LunchMinutes_Previews: PreviewProvider {
     }
 }
 
-#if os(iOS)
-struct TestTextfield: UIViewRepresentable {
-    @Binding var text: String
-    var keyType: UIKeyboardType
-    func makeUIView(context: Context) -> UITextField {
-        let textfield = UITextField()
-      textfield.keyboardType = keyType
-        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: textfield.frame.size.width, height: 44))
-        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(textfield.doneButtonTapped(button:)))
-        toolBar.items = [doneButton]
-        toolBar.setItems([doneButton], animated: true)
-        textfield.inputAccessoryView = toolBar
-        return textfield
-    }
-    
-    func updateUIView(_ uiView: UITextField, context: Context) {
-        uiView.text = text
-        
-    }
-}
-
-extension  UITextField{
-    @objc func doneButtonTapped(button:UIBarButtonItem) -> Void {
-       self.resignFirstResponder()
-    }
-
-}
-#endif
+//#if os(iOS)
+//struct TestTextfield: UIViewRepresentable {
+//    @Binding var text: String
+//    var keyType: UIKeyboardType
+//    func makeUIView(context: Context) -> UITextField {
+//        let textfield = UITextField()
+//        textfield.font = UIFont.systemFont(ofSize: 20)
+////        print(UIFont.familyNames)
+//        textfield.placeholder = "30"
+//      textfield.keyboardType = keyType
+//        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: textfield.frame.size.width, height: 44))
+//        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(textfield.doneButtonTapped(button:)))
+//        toolBar.items = [doneButton]
+//        toolBar.setItems([doneButton], animated: true)
+//        textfield.inputAccessoryView = toolBar
+//        return textfield
+//    }
+//
+//    func updateUIView(_ uiView: UITextField, context: Context) {
+//        uiView.text = text
+//
+//    }
+//}
+//
+//extension  UITextField{
+//    @objc func doneButtonTapped(button:UIBarButtonItem) -> Void {
+//       self.resignFirstResponder()
+//    }
+//
+//}
+//#endif
